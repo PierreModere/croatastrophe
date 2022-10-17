b793b6ec52194327a38daa4d29af1176f947bd09
pierremodere <pierre.martiniere72@gmail.com>
2022-10-17 15:31:53 +0200

    First commit ^3^

---
+++ sources/Experience/World.js
@@ -0,0 +1,47 @@
+import * as THREE from 'three'
+import Experience from './Experience.js'
+
+export default class World
+{
+    constructor(_options)
+    {
+        this.experience = new Experience()
+        this.config = this.experience.config
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+        
+        this.resources.on('groupEnd', (_group) =>
+        {
+            if(_group.name === 'base')
+            {
+                this.setDummy()
+            }
+        })
+    }
+
+    setDummy()
+    {
+        this.resources.items.lennaTexture.encoding = THREE.sRGBEncoding
+        
+        const cube = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshBasicMaterial({ map: this.resources.items.lennaTexture })
+        )
+
+        this.scene.add(this.resources.items.shibaModel.scene)
+        // this.scene.add(cube)        
+    }
+
+    resize()
+    {
+    }
+
+    update()
+    {
+        console.log(this.experience.time.elapsed/1000)
+    }
+
+    destroy()
+    {
+    }
+}
---
+++ sources/Experience/assets.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name: 'base',
+        data: {},
+        items:
+        [
+            { name: 'lennaTexture', source: '/assets/lenna.png', type: 'texture' },
+            { name: 'shibaModel', source: '/assets/shiba/shiba.gltf', type: 'model' },
+
+        ]
+    }
+]
